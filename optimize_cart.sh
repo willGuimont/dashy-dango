7464ba59976b8f01c74c373182defd51e7eb607b
William Guimont-Martin <wil9876543210@hotmail.com>
2022-07-26 18:06:50 -0400

    added wasm-snip option

--- optimize_cart.sh
+++ optimize_cart.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code target/wasm32-unknown-unknown/release/cart.wasm -o target/wasm32-unknown-unknown/release/dashy-dango.wasm
+wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code --skip-producers-section target/wasm32-unknown-unknown/release/cart.wasm -o target/wasm32-unknown-unknown/release/dashy-dango.wasm
 wasm-opt -Oz --converge --strip-producers --strip-debug --dce --strip-dwarf --strip-target-features -all --ignore-implicit-traps --const-hoisting target/wasm32-unknown-unknown/release/dashy-dango.wasm -o target/wasm32-unknown-unknown/release/dashy-dango.wasm
