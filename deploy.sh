70183f5542d0768ab21fc6477ea2356aa985a3ab
William Guimont-Martin <wil9876543210@hotmail.com>
2022-07-25 20:49:35 -0400

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 cargo build --release
-w4 bundle --html build/html/index.html --title "Dashy Dango" --description "Dashy game" --icon-file "assets/sprites/dangoBeeg.png" target/wasm32-unknown-unknown/release/cart.wasm
+wasm-opt -Oz --converge --strip-producers --strip-debug --strip-dwarf --strip-target-features -all --ignore-implicit-traps --const-hoisting target/wasm32-unknown-unknown/release/cart.wasm -o build/cart.wasm
+w4 bundle --html build/html/index.html --title "Dashy Dango" --description "Dashy game" --icon-file "assets/sprites/dangoBeeg.png" build/cart.wasm
 git add -f build/html && git commit -m "Deploy"
-git push origin `git subtree split --prefix build/html`:gh-pages --force
+git push origin `git subtree split --prefix build/html`:refs/heads/gh-pages --force
