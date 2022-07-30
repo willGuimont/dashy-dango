93c405b909a1738997e4421d01ab8ce449ac4fad
William Guimont-Martin <wil9876543210@hotmail.com>
2022-07-29 23:50:55 -0400

    some more improvements

--- src/game/systems/enemy_system.rs
+++ src/game/systems/enemy_system.rs
@@ -13,19 +13,15 @@ impl EnemySystem {
 
 impl System for EnemySystem {
     fn execute_system(&mut self, registry: &mut Registry) {
-        let mut to_destroy = Vec::new();
         for e in entities_with!(registry, EnemyComponent) {
             let (mut enemy, ) = get_components_clone_unwrap!(registry, e, EnemyComponent);
 
             if enemy.time_to_live <= 0 {
-                to_destroy.push(e);
+                registry.destroy_entity(e);
             } else {
                 enemy.time_to_live -= 1;
             }
             registry.add_component(e, enemy).abort();
-        }
-        for e in to_destroy {
-            registry.destroy_entity(e);
         }
     }
 }
