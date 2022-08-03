3bef3a84fd14cc57472ec1c95450d3e747dd3642
William Guimont-Martin <wil9876543210@hotmail.com>
2022-08-03 16:37:14 -0400

    child component

---
+++ src/game/systems/child_system.rs
@@ -0,0 +1,21 @@
+use crate::{Abort, entities_with, get_components_clone_unwrap, has_all_components, Registry};
+use crate::ecs::Entity;
+use crate::game::components::{ChildComponent, PositionComponent};
+use crate::game::systems::System;
+
+pub struct ChildSystem;
+
+impl System for ChildSystem {
+    fn execute_system(&mut self, registry: &mut Registry) {
+        for e in entities_with!(registry, ChildComponent, PositionComponent) {
+            let (child, mut pos) = get_components_clone_unwrap!(registry,e, ChildComponent, PositionComponent);
+            if !registry.is_alive(child.parent) {
+                registry.destroy_entity(e);
+            }
+            if let Some(parent_pos) = registry.get_component::<PositionComponent>(child.parent) {
+                pos.pos = parent_pos.pos + child.relative_pos;
+                registry.add_component(e, pos);
+            }
+        }
+    }
+}
