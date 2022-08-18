d5eccfcadced911aada7009fabb30137f6af9ee4
William Guimont-Martin <wil9876543210@hotmail.com>
2022-08-18 12:57:02 -0400

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ cp target/wasm32-unknown-unknown/release/dashy-dango.wasm build/cart.wasm
 w4 bundle --html build/html/index.html --title "Dashy Dango" --description "Dashy game" --icon-file "assets/sprites/dangoBeeg.png" build/cart.wasm
 git add -f build/ && git commit -m "Deploy"
 git push origin $(git subtree split --prefix build/html):refs/heads/gh-pages --force
+git push
+
